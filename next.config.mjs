a56c0fae7a2785743139e0f09d552cf7a7a5e459
“BeeRad” <bradwmorriswork@gmail.com>
2024-11-13 19:01:30 +1100

    update next config to ignore eslint during build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig;
